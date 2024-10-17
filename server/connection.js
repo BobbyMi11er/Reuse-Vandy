348a84af95d14ceef5cf1536e9caa63f1a89b49d
Bobby Mi11er <bobby@tybeemillers.com>
2024-10-17 17:46:58 -0500

    Update schema for string firebase id

--- server/connection.js
+++ server/connection.js
@@ -2,12 +2,13 @@ require('dotenv').config()
 
 const mysql = require('mysql')
 
-const conn = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.HOST_URL,
     user: process.env.USER,
     password: process.env.PASSWORD,
     timeout: 15,
-    multipleStatements: true
+    multipleStatements: true,
+    connectionLimit: 10,
 })
 
 createSchemaQuery = `
@@ -20,13 +21,13 @@ createUserTableQuery = `
     DROP TABLE IF EXISTS User;
 
     CREATE TABLE User (
-        \`user_id\` INT NOT NULL,
+        \`user_firebase_id\` VARCHAR(128) NOT NULL,
         \`name\` VARCHAR(45) DEFAULT '',
         \`pronouns\` VARCHAR(12) DEFAULT '',
         \`email\` VARCHAR(45) DEFAULT '',
         \`phone_number\` VARCHAR(12) DEFAULT '',
         \`profile_img_url\` VARCHAR(255) DEFAULT '',
-        PRIMARY KEY (\`user_id\`)
+        PRIMARY KEY (\`user_firebase_id\`)
     );
 `
 
@@ -34,42 +35,48 @@ createPostTableQuery = `
     DROP TABLE IF EXISTS Post;
 
     CREATE TABLE Post (
-        \`user_id\` INT NOT NULL,
-        \`post_id\` INT NOT NULL,
-        \`image_url\` VARCHAR(255) DEFAULT '',
-        \`title\` VARCHAR(20) DEFAULT '',
-        \`description\` VARCHAR(255) DEFAULT '',
-        \`color\` VARCHAR(20) DEFAULT '',
-        \`attributes\` VARCHAR(255) DEFAULT '',
-        CONSTRAINT \`post_user_constraint\`
-            FOREIGN KEY (\`user_id\`)
-            REFERENCES \`User\` (\`user_id\`)
-            ON DELETE CASCADE
-    );
+    \`post_id\` INT NOT NULL AUTO_INCREMENT,
+    \`user_firebase_id\` VARCHAR(128) NOT NULL,
+    \`image_url\` VARCHAR(255) DEFAULT '',
+    \`title\` VARCHAR(100) NOT NULL,
+    \`description\` VARCHAR(500) DEFAULT '',
+    \`color\` VARCHAR(30) DEFAULT '',
+    \`attributes\` JSON DEFAULT NULL,
+    \`created_at\` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    \`updated_at\` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (\`post_id\`),
+    CONSTRAINT \`post_user_constraint\`
+        FOREIGN KEY (\`user_firebase_id\`)
+        REFERENCES \`User\` (\`user_firebase_id\`)
+        ON DELETE CASCADE,
+    INDEX idx_title (\`title\`),
+    INDEX idx_color (\`color\`)
+);
 `
 
 const createTables = () => {
-    conn.connect(function(err) {
+    pool.connect(function(err) {
         if (err) throw err;
         console.log("Connected! Creating tables...");
 
-        conn.query(createSchemaQuery, function (err, result) {
+        pool.query(createSchemaQuery, function (err, result) {
             if (err) throw err;
             console.log("Schema created");
         });
 
-        conn.query(createUserTableQuery, function (err, result) {
+        pool.query(createUserTableQuery, function (err, result) {
             if (err) throw err;
             console.log("User table created");
         });
 
-        conn.query(createPostTableQuery, function (err, result) {
+        pool.query(createPostTableQuery, function (err, result) {
             if (err) throw err;
             console.log("Post table created");
+            console.log(result)
         });
     });
 }
 
-// createTables();
+createTables();
 
-module.exports = conn;
+// module.exports = conn;
